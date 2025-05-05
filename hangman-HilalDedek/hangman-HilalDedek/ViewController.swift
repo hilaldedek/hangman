9e6b085ef8ed1616c3ada59154e291a2e6f8222b
hilaldedek <hdedek03@gmail.com>
2025-05-05 13:06:17 +0300

    feat: initalize project.

---
+++ hangman-HilalDedek/hangman-HilalDedek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hangman-HilalDedek
+//
+//  Created by Trakya12 on 5.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
